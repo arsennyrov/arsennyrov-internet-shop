b9f130bcfaf306e6e8ea485cd70e7939a0cfadaf
arsennyrov <arsenyrov@mail.ru>
2023-02-02 16:27:46 +0300

    Updates

---
+++ precache-manifest.adaf7baf3adcff8596a7912bf8365b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17eacfaf0e8abe77a620fa459f4d217",
+    "url": "/internet-shop/index.html"
+  },
+  {
+    "revision": "6d1e273fb47cb024d1ac",
+    "url": "/internet-shop/static/css/main.3312dde5.chunk.css"
+  },
+  {
+    "revision": "89a72705f3739c063b47",
+    "url": "/internet-shop/static/js/2.f5f5a089.chunk.js"
+  },
+  {
+    "revision": "6d1e273fb47cb024d1ac",
+    "url": "/internet-shop/static/js/main.286cf429.chunk.js"
+  },
+  {
+    "revision": "27609a7b8a5f1a3784dd",
+    "url": "/internet-shop/static/js/runtime~main.b2ae3f2c.js"
+  }
+]);
